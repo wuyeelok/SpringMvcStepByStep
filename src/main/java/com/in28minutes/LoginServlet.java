ae6c8304a98972994f814de2e6585813b44a1d7c
wuyeelok <wuyeelok@gmail.com>
2019-04-30 14:23:01 +0800

    Use HTTP Get method

--- src/main/java/com/in28minutes/LoginServlet.java
+++ src/main/java/com/in28minutes/LoginServlet.java
@@ -16,7 +16,10 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+
 		request.setAttribute("name", name);
+		request.setAttribute("password", password);
 		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
 	}
 
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -1,19 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-<%@ page import="java.util.Date"%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Yahoo From JSP</title>
 </head>
-<%
-	System.out.println("sds");
-	Date date = new Date();
-%>
+
 <body>
 	<h1>My Second JSP</h1>
-	<p>Welcome ${name}</p>
-	<p><%=date%></p>
+	<p>Welcome ${name}, your password is ${password}</p>
+
 </body>
 </html>
