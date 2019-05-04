5d7ef4bb7484af3d7face33665592f616ab86c29
wuyeelok <wuyeelok@gmail.com>
2019-05-04 20:57:38 +0800

    Login post submit use spring to handle

--- src/main/java/com/in28minutes/springmvc/LoginController.java
+++ src/main/java/com/in28minutes/springmvc/LoginController.java
@@ -2,12 +2,18 @@ package com.in28minutes.springmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping(value = "/login")
-	public String sayHello() {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLoginPage() {
 		return "login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String handleLoginRequest() {
+		return "welcome";
 	}
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -11,7 +11,7 @@
 <body>
 	<h1>My Second JSP</h1>
 	<p>${errorMessage}</p>
-	<form action="/login.do" method="post">
+	<form action="/spring-mvc/login" method="post">
 		<label for="name">Enter your name</label><br /> <input type="text"
 			id="name" name="name" /><br /> <label for="password">Enter
 			your password</label><br /> <input type="password" id="password"
