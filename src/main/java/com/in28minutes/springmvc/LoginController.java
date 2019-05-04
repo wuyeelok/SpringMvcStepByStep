f3566519225fa187daae6a24a0af93f8e33d7cfd
wuyeelok <wuyeelok@gmail.com>
2019-05-04 21:15:42 +0800

    Set param name into Spring Model

--- src/main/java/com/in28minutes/springmvc/LoginController.java
+++ src/main/java/com/in28minutes/springmvc/LoginController.java
@@ -1,8 +1,10 @@
 package com.in28minutes.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -13,7 +15,8 @@ public class LoginController {
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String handleLoginRequest() {
+	public String handleLoginRequest(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
 		return "welcome";
 	}
 }
