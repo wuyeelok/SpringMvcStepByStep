bf23e7ab4b5534468ae398ee9f78a945739d200b
wuyeelok <wuyeelok@gmail.com>
2019-05-06 15:58:10 +0800

    Use model clear and redirect

--- src/main/java/com/in28minutes/todo/TodoController.java
+++ src/main/java/com/in28minutes/todo/TodoController.java
@@ -1,10 +1,13 @@
 package com.in28minutes.todo;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -15,9 +18,22 @@ public class TodoController {
 	TodoService service;
 
 	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
-	public String listTodoPage(ModelMap model) {
+	public String listTodos(ModelMap model) {
 		String name = String.valueOf(model.get("name"));
 		model.addAttribute("todos", service.retrieveTodos(name));
 		return "list-todos";
 	}
-}
+
+	@RequestMapping(value = "/add-todo", method = RequestMethod.GET)
+	public String showTodoPage() {
+		return "todo";
+	}
+
+	@RequestMapping(value = "/add-todo", method = RequestMethod.POST)
+	public String addTodo(@RequestParam String desc, ModelMap model) {
+		service.addTodo(String.valueOf(model.get("name")), desc, new Date(), false);
+
+		model.clear();
+		return "redirect:/list-todos";
+	}
+};
--- src/main/webapp/WEB-INF/views/todo.jsp
+++ src/main/webapp/WEB-INF/views/todo.jsp
@@ -1,17 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>List Todos JSP</title>
+<title>Add Todo Page</title>
 </head>
-
 <body>
-	<h1>Hi ${name}</h1>
-	<p>Your Todos are ${todos}</p>
-	<br />
-	<a href="/add-todo">Add</a>
+	<h1>Add a Todo</h1>
+	<form method="post">
+		<input name="desc" type="text" /> <input type="submit" value="Add" />
+	</form>
 </body>
 </html>
