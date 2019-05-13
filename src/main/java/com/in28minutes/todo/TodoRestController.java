6ecf8fd2a68945f2964bcef0301d31f96ae4d3c9
wuyeelok <wuyeelok@gmail.com>
2019-05-13 18:13:03 +0800

    Use @PathVariable for rest web service

--- src/main/java/com/in28minutes/todo/TodoRestController.java
+++ src/main/java/com/in28minutes/todo/TodoRestController.java
@@ -3,6 +3,7 @@ package com.in28minutes.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,10 @@ public class TodoRestController {
 	@RequestMapping(value = "show-all-t", method = RequestMethod.GET)
 	public List<Todo> retrieveAllTodos() {
 		return service.retrieveTodos("tom");
+	}
+
+	@RequestMapping(value = "show-all-t/{id}", method = RequestMethod.GET)
+	public Todo retrieveTodo(@PathVariable int id) {
+		return service.retrieveTodo(id);
 	}
 }
